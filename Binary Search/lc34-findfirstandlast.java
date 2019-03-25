e5cf897e9e4d16862b454189a444149137da1578
Jiahui Li <jhlaboutcs@gmail.com>
2019-03-24 22:24:43 -0700

    Update and rename lc34.java to lc34-findfirstandlast.java

--- Binary Search/lc34-findfirstandlast.java
+++ Binary Search/lc34-findfirstandlast.java
@@ -1,3 +1,11 @@
+/**
+Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+If the target is not found in the array, return [-1, -1].
+*/
+
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int start = 0;
