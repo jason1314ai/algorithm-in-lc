b5019d948b8bfe39588035fa2f6cf8dde1d031ee
Jiahui Li <jhlaboutcs@gmail.com>
2019-03-25 17:47:42 -0700

    Update lc153-154-min_in_rotated_array.java

--- Binary Search/lc153-154-min_in_rotated_array.java
+++ Binary Search/lc153-154-min_in_rotated_array.java
@@ -6,7 +6,7 @@ Suppose an array sorted in ascending order is rotated at some pivot unknown to y
 Find the minimum element.
 */
 
-
+ // no duplicate number
 class Solution {
     public int findMin(int[] nums) {
         if(nums == null || nums.length ==0)
@@ -22,6 +22,38 @@ class Solution {
             else 
                 end = mid;
 
+        }
+        return Math.min(nums[start],nums[end]);
+    }
+}
+
+
+// duplicate number
+
+class Solution {
+    public int findMin(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return -1;
+        int start = 0;
+        int end = nums.length -1;
+        while(start+1 < end){
+            int mid = (end -start)/2 +start;
+            if(nums[mid]<nums[mid -1])
+                return nums[mid];
+            if(nums[mid] == nums[start] && nums[mid] == nums[end]){
+                start ++;
+                end --;
+            }
+            else if(nums[mid] == nums[start])
+                start = mid;
+            else if(nums[mid] == nums[end])
+                end = mid;
+            else{
+                if(nums[mid]<nums[end])
+                    end = mid;
+                else
+                    start = mid;
+            }
         }
         return Math.min(nums[start],nums[end]);
     }
