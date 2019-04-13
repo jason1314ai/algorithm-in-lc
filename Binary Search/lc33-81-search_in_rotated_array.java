3e7a407966c14dd3bd5bf681d80d23a9ca660d23
Jiahui Li <jhlaboutcs@gmail.com>
2019-04-13 03:05:01 -0700

    Update lc33-81-search_in_rotated_array.java

--- Binary Search/lc33-81-search_in_rotated_array.java
+++ Binary Search/lc33-81-search_in_rotated_array.java
@@ -54,48 +54,43 @@ class Solution {
 // duplicate
 class Solution {
     public boolean search(int[] nums, int target) {
-        if(nums == null || nums.length ==0)
+        if(nums == null || nums.length == 0)
             return false;
         int start = 0;
-        int end = nums.length - 1;
+        int end = nums.length-1;
         while(start+1 < end){
-            int mid = (end-start)/2+start;
+            int mid = (end- start)/2 + start;
             if(nums[mid] == target)
                 return true;
-            if(nums[mid] == nums[start] && nums[mid] == nums[end]){
-                start ++ ;
-                end --;
-            }
+           if(nums[mid] == nums[end] && nums[mid] == nums[start])
+               end --;
             else if(nums[mid] == nums[start])
-                start = mid +1;
+                start = mid + 1;
             else if(nums[mid] == nums[end])
                 end = mid -1;
-            else{
-                if(nums[mid] < nums[end]){
-                    if(nums[mid]>target){
-                        end = mid -1;
-                    }
-                    else{
-                        if(nums[end] >= target)
-                            start =mid +1;
-                        else
-                            end = mid -1;
-                    }
-                }else{
-                    if(nums[mid]>target){
-                        if(nums[end] >= target)
-                            start = mid +1;
-                        else
-                            end = mid -1;
-                    }
-                    else{
-                        start = mid +1;
-                    }
+            else if(nums[end] > nums[mid]){
+                if(target <= nums[end]){
+                    if(target > nums[mid])
+                        start = mid;
+                    else
+                        end = mid;
+                }
+                else
+                    end = mid;
+            }
+            else if(nums[mid] > nums[end]){
+                if(target <= nums[end])
+                    start = mid;
+                else{
+                    if(nums[mid]>target)
+                        end = mid;
+                    else
+                        start = mid;
                 }
             }
         }
-        if(nums[start] == target || nums[end] == target)
-            return true;
-        return false;
+        if(nums[start] != target && nums[end] != target)
+            return false;
+        return true;
     }
 }
