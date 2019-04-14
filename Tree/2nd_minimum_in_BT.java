df28b8839536d28315bcef030b10cf07340edf04
Jiahui Li <jhlaboutcs@gmail.com>
2019-04-14 01:20:51 -0700

    Update 2nd_minimum_in_BT.java

--- Tree/2nd_minimum_in_BT.java
+++ Tree/2nd_minimum_in_BT.java
@@ -9,10 +9,7 @@
  */
  
  /*
-（1）如果根结点或者左右都为空，则不存在；
- (2) 如果左子节点等于root，右子节点不等， 则求Math.min(f(left) == -1,则返回右,right);
- (3) 如果右子节点等于root, 左子节点不等， 则求Math.min(f(right) == -1则返回左,left);
- (4) 都等于的情况下，求Math.min(f(left),f(right) 看谁为-1);
+找寻左右子节点第一个不为根结点的数，再比较大小；
  */
 class Solution {
     public int findSecondMinimumValue(TreeNode root) {
